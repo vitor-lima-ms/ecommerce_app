6eaa1ca9046f2c0b636cf8212f46fdb24a445dd8
Vitor Lima <viitorlima2000@gmail.com>
2025-04-11 22:12:38 -0300

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from orders.models import ItemOrdered
 from orders.my_forms import OrdersForm
 from cart.cart import Cart
+# PayPal
+from django.urls import reverse
+from paypal.standard.forms import PayPalPaymentsForm
+from django.conf import settings
+import uuid
 
 # Create your views here.
 
@@ -21,10 +26,30 @@ def order_create(request):
                     price=item['price'],
                     qtd=item['purchase_qtd'],
                 )
+
+            # PayPal
+            host = request.get_host()
+            paypal_dict = {
+                'business': settings.PAYPAL_RECEIVER_EMAIL,
+                'amount': cart.get_grand_total(),
+                'item_name': 'Pedido',
+                'no_shipping': '2',
+                'invoice': str(uuid.uuid4()),
+                'currency_code': 'BRL',
+                'notify_url': 'https://{}{}'.format(host, reverse('orders:paypal-ipn')),
+                'return_url': 'https://{}{}'.format(host, reverse('orders:payment_success')),
+                'cancel_return': 'https://{}{}'.format(host, reverse('orders:payment_failed')),
+            }
+
+            paypal_form = PayPalPaymentsForm(initial=paypal_dict)
+            context = {
+                'form': paypal_form,
+                'order': order,
+            }
             
             cart.clean_cart(request)
 
-            return render(request, 'order_complete.html', {'order': order})
+            return render(request, 'order_complete.html', context)
     
     else:
         form = OrdersForm()
@@ -32,3 +57,9 @@ def order_create(request):
             'cart': cart,
             'form': form,
         })
+
+def payment_success(request):
+    return render(request, 'payment_success.html')
+
+def payment_failed(request):
+    return render(request, 'payment_failed.html')
