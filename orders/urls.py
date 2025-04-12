1884e2376891554a27e860a6045ba4757f2a1c80
Vitor Lima <viitorlima2000@gmail.com>
2025-04-11 22:12:39 -0300

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from orders import views
 
 app_name = 'orders'
 
 urlpatterns = [
     path('', views.order_create, name='order_create'),
+
+    path('paypal/', include('paypal.standard.ipn.urls')),
+
+    path('payment_success/', views.payment_success, name='payment_success'),
+
+    path('payment_failed/', views.payment_failed, name='payment_failed'),
 ]
